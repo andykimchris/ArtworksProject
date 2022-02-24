574e02de57bb69b4b6dc5c88f15bd25ca98aca5e
andykimchris <andrew.muchiri97@gmail.com>
2022-02-24 21:00:54 +0300

    default state for checkboxes

---
+++ src/config/checkboxes.ts
@@ -0,0 +1,41 @@
+import { checkBoxInterface } from "../utilities/modelInterfaces";
+
+const checkboxes: checkBoxInterface[] = [
+  {
+    id: "People",
+    label: "People",
+    checked: false,
+  },
+  {
+    id: "Premium",
+    label: "Premium",
+    checked: false,
+  },
+  {
+    id: "Pets",
+    label: "Pets",
+    checked: false,
+  },
+  {
+    id: "Food",
+    label: "Food",
+    checked: false,
+  },
+  {
+    id: "Landmarks",
+    label: "Landmarks",
+    checked: false,
+  },
+  {
+    id: "Cities",
+    label: "Cities",
+    checked: false,
+  },
+  {
+    id: "Nature",
+    label: "Nature",
+    checked: false,
+  },
+];
+
+export default checkboxes;
