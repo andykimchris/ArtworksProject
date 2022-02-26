17b638e66a4432ce049e86687aae53a3e7470c6a
andykimchris <andrew.muchiri97@gmail.com>
2022-02-26 19:40:47 +0300

    arrow vectors

---
+++ src/vectors/ArrowLeft.tsx
@@ -0,0 +1,15 @@
+const ArrowLeft: React.FC = () => {
+  return (
+    <svg
+      width="13"
+      height="20"
+      viewBox="0 0 13 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M11 2L3 10L11 18" stroke="black" strokeWidth="3" />
+    </svg>
+  );
+};
+
+export default ArrowLeft;
---
+++ src/vectors/ArrowRight.tsx
@@ -0,0 +1,15 @@
+const ArrowRight: React.FC = () => {
+  return (
+    <svg
+      width="13"
+      height="20"
+      viewBox="0 0 13 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M2 2L10 10L2 18" stroke="black" strokeWidth="3" />
+    </svg>
+  );
+};
+
+export default ArrowRight;
