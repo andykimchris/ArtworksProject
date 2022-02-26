3f24963b7681b3ee8d36e31b48769e7a833335f0
andykimchris <andrew.muchiri97@gmail.com>
2022-02-26 15:37:25 +0300

    added arrow vectors & installed axios

---
+++ src/vectors/ArrowDown.tsx
@@ -0,0 +1,15 @@
+const ArrowDown: React.FC = () => {
+  return (
+    <svg
+      width="20"
+      height="13"
+      viewBox="0 0 20 13"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M2 2L10 10L18 2" stroke="black" strokeWidth="3" />
+    </svg>
+  );
+};
+
+export default ArrowDown;
