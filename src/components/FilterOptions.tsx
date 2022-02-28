2003a948c38466f4de05621e7db1b37aae1e05d0
andykimchris <andrew.muchiri97@gmail.com>
2022-02-28 12:20:23 +0300

    set price range based on label

--- src/components/FilterOptions.tsx
+++ src/components/FilterOptions.tsx
@@ -1,11 +1,18 @@
 import { useContext } from "react";
 import { Form } from "react-bootstrap";
-import CheckBox from "./Checkbox";
+import CategoryCheckBox from "./CategoryCheckbox";
 import { ArtContext } from "../contexts/ArtGalleryContext";
+import PriceCheckBox from "./PriceCheckBox";
 
 const FilterOptions = () => {
-  const { totalItems, updatedCheckBoxItem, filteredItems } =
-    useContext(ArtContext);
+  const {
+    totalItems,
+    updatedCheckBoxItem,
+    filteredItems,
+    handleFilteredPriceRange,
+    priceRangeItem,
+    handlePriceRangeItem,
+  } = useContext(ArtContext);
 
   const handleCheckChange = (e: React.ChangeEvent<any>) => {
     let { id, checked } = e.target;
@@ -14,15 +21,41 @@ const FilterOptions = () => {
     filteredItems(id, checked);
   };
 
-  const handleChange = () => console.log("");
+  const handleFilterPriceChange = (
+    e: React.ChangeEvent<any>,
+    priceRange: string
+  ) => {
+    let { id, checked } = e.target;
+    handlePriceRangeItem(id, checked);
+
+    if (!checked) {
+      handleFilteredPriceRange(0);
+    } else {
+      switch (priceRange) {
+        case "Lower than $20":
+          handleFilteredPriceRange(19.99);
+          break;
+        case "$20 - $100":
+          handleFilteredPriceRange(99.99);
+          break;
+        case "$100 - $200":
+          handleFilteredPriceRange(199.99);
+          break;
+        case "More than $200":
+          handleFilteredPriceRange(200);
+          break;
+      }
+    }
+  };
+
   return (
     <Form>
-      {totalItems.map((item) => (
-        <CheckBox
-          label={item.label}
-          key={item.id}
-          id={item.id}
-          checked={item.checked}
+      {totalItems.map((filter) => (
+        <CategoryCheckBox
+          label={filter.label}
+          key={filter.id}
+          id={filter.id}
+          checked={filter.checked}
           onChange={handleCheckChange}
         />
       ))}
@@ -30,38 +63,15 @@ const FilterOptions = () => {
       <hr />
 
       <h2>Price Range</h2>
-
-      <Form.Group className="mb-3 checkbox-group" controlId="formBasicCheckbox">
-        <Form.Check
-          type="checkbox"
-          label="Lower than $20"
-          onChange={handleChange}
+      {priceRangeItem.map((filter) => (
+        <PriceCheckBox
+          label={filter.label}
+          key={filter.id}
+          id={filter.id}
+          checked={filter.checked}
+          onChange={(e) => handleFilterPriceChange(e, filter.label)}
         />
-      </Form.Group>
-
-      <Form.Group className="mb-3 checkbox-group" controlId="formBasicCheckbox">
-        <Form.Check
-          type="checkbox"
-          label="$20 - $100"
-          onChange={handleChange}
-        />
-      </Form.Group>
-
-      <Form.Group className="mb-3 checkbox-group" controlId="formBasicCheckbox">
-        <Form.Check
-          type="checkbox"
-          label="$100 - $200"
-          onChange={handleChange}
-        />
-      </Form.Group>
-
-      <Form.Group className="mb-3 checkbox-group" controlId="formBasicCheckbox">
-        <Form.Check
-          type="checkbox"
-          label="More than $200"
-          onChange={handleChange}
-        />
-      </Form.Group>
+      ))}
     </Form>
   );
 };
